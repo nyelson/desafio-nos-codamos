9f7a147459e0d5b4edee7ae5d6beb055f01d600a
nyelson.barbosa <nyelson.barbosa@nubank.com.br>
2021-07-29 16:44:16 -0300

    Associando os símbolos entre Cliente -> Cartão -> Compras.

--- src/desafio_nos_codamos/core.clj
+++ src/desafio_nos_codamos/core.clj
@@ -1,17 +1,15 @@
 (ns desafio-nos-codamos.core)
 
+(refer-clojure :exclude [range iterate format max min])
+(use 'java-time)
+
 (println "Início do projeto - Core")
 
-(def cliente  {:nome "Almerindo", :cpf "22667862023", :email "almerindo@nubank.com.br"})
-(def cartao   {:numero "5410133996442556", :cvv "984", :validade "30/12/2022" , :limite 10000}) ;(java.time.LocalDateTime/now)
-(def compras  [ {:data "30/12/2022", :valor 1000,  :estabelecimento "Adidas",        :categoria "Vestuário"},
-                {:data "30/12/2022", :valor 40,    :estabelecimento "Burguer King",  :categoria "Restaurante"},
-                {:data "30/12/2022", :valor 300,   :estabelecimento "Nike",          :categoria "Vestuário"}])
-
-;criar uma função para retornar o group by
-;criar uma função para pegar o simbolo específico na lista agrupada
-;colocar o resultado em um simbolo de agrupamento
-;criar uma função para retornar por categoria
+(def cliente  { :nome "Almerindo", :cpf "22667862023", :email "almerindo@nubank.com.br"})
+(def cartao   { :numero "5410133996442556", :cvv "984", :validade (format "dd/MM/yyyy" (zoned-date-time 2022 10 20)), :limite 10000}) ;(java.time.LocalDateTime/now)
+(def compras  [ { :data (format "dd/MM/yyyy" (zoned-date-time 2021 10 20)), :valor 1000,  :estabelecimento "Adidas",        :categoria "Vestuário"   },
+                { :data (format "dd/MM/yyyy" (zoned-date-time 2021 03 13)), :valor 40,    :estabelecimento "Burguer King",  :categoria "Restaurante" },
+                { :data (format "dd/MM/yyyy" (zoned-date-time 2021 05 29)), :valor 300,   :estabelecimento "Nike",          :categoria "Vestuário"   } ])
 
 (defn agrupa-categorias
   "Agrupa e retorna uma lista de acordo com a categoria"
@@ -27,15 +25,12 @@
   [lista]
   (->> lista (map :valor) (reduce +)))
 
-(def compras-categorizadas (agrupa-categorias compras))
-(def lista-vestuario       (retorna-simbolo-por-categoria compras-categorizadas "Vestuário"))
-(def lista-restaurante     (retorna-simbolo-por-categoria compras-categorizadas "Restaurante"))
+(def compras-categorizadas  (agrupa-categorias compras))
+(def lista-vestuario        (retorna-simbolo-por-categoria compras-categorizadas "Vestuário"))
+(def lista-restaurante      (retorna-simbolo-por-categoria compras-categorizadas "Restaurante"))
 
 (println "O total das compras referente a categoria vestuário é de:" (somatorio-total lista-vestuario))
 (println "O total das compras referente a categoria restaurante é de:" (somatorio-total lista-restaurante))
 (println "O total das compras referente fatura é de:" (somatorio-total compras))
 
-;(println (assoc-in cartao compras (map compras)))
-;(println (conj cartao 1))
-;(println (def cartao (assoc cartao compras (map compras))))
-;(println cartao)
+(println (assoc-in cliente [:cartaoRef] (assoc-in cartao [:comprasRef] compras)))
